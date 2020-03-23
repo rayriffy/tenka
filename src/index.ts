417604ae6e909f72719e39996c4473c901ce4e0f
Phumrapee Limpianchop <git@rayriffy.com>
2020-03-24 00:37:59 +0700

    feat!: intitial c

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { Client } from 'discord.js'
+import http from 'http'
+
+import { isNumber } from 'lodash-es'
+
+import { getHentaiById } from './services/getHentaiById'
+
+const client = new Client()
+const { AUTH_TOKEN = '' } = process.env
+
+client.on('ready', () => {
+  console.log('Ready!')
+})
+
+client.on('message', async message => {
+  if (message.content?.startsWith('!tenka ')) {
+    const query = message.content.substr(7)
+    
+    switch(query.split(' ')[0]) {
+      case 'nh':
+        const key = query.split(' ')[1].toLocaleLowerCase()
+        
+        if (key === 'rand' || key === 'random') {
+          // Random nh
+          try {
+            const hentai = await getHentaiById((Math.floor(Math.random() * 100000000000) % 350000) - 1)
+
+            if (message.channel !== null) message.channel.send(hentai)
+          } catch {
+            if (message.reply !== null) message.reply(`Could not get data properly from server`)
+          }
+        } else if (isNumber(key)) {
+          // Get from ID
+          try {
+            const hentai = await getHentaiById(key)
+
+            if (message.channel !== null) message.channel.send(hentai)
+          } catch {
+            if (message.reply !== null) message.reply(`Could not find hentai in NHentai with ID ${key}`)
+          }
+        } else {
+          // Search
+          if (message.reply !== null) message.reply('Sorry, search hentai by keyword feature is not available')
+        }
+        
+        break
+      default:
+        if (message.reply !== null) message.reply('Command not found')
+        break
+    }
+  }
+})
+
+client.login(AUTH_TOKEN)
+
+http.createServer().listen(3000)
---
+++ src/services/getHentaiById.ts
@@ -0,0 +1,30 @@
+import { MessageEmbed } from 'discord.js'
+
+import { upperFirst } from 'lodash-es'
+
+import { fetch } from '../core/fetch'
+
+import { IAPIResponse } from '../@types/IAPIResponse'
+import { IHentai } from '../@types/IHentai'
+
+export const getHentaiById = async (id: string | number): Promise<MessageEmbed> => {
+  try {
+    const out = await fetch<IAPIResponse<IHentai>>(
+      `https://h.api.rayriffy.com/v1/gallery/${id}`
+    )
+
+    const hentai = out.response.data
+
+    const payload = new MessageEmbed()
+      .setTitle(hentai.title.pretty)
+      .setDescription(hentai.title.japanese)
+      .addField('Page', hentai.images.pages.length)
+      .addField('Language', hentai.tags.filter(o => o.type === 'language')[0] ? upperFirst(hentai.tags.filter(o => o.type === 'language')[0].name) : 'Unknown')
+      .setImage(`https://t.nhentai.net/galleries/${hentai.media_id}/cover.${hentai.images.cover.t === 'p' ? 'png' : 'jpg'}`)
+      .setURL(`https://h.rayriffy.com/g/${hentai.id}`)
+
+    return payload
+  } catch (e) {
+    throw e
+  }
+}
